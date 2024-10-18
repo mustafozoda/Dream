216cefa1d417fff7db7ab0e6d73853750b912e63
mustafozoda <mustafozodashq888@gmail.com>
2024-10-18 12:41:18 +0200

    d

--- Projects/company/server/src/main/java/com/mustaffozoda/dev/company/exception/GlobalExceptionHandler.java
+++ Projects/company/server/src/main/java/com/mustaffozoda/dev/company/exception/GlobalExceptionHandler.java
@@ -7,11 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import org.springframework.dao.DataIntegrityViolationException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return new ResponseEntity<>("Data integrity violation: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
